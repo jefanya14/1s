01e036d666208c3e32bb1e34fe531a11a2b648ef
Jefanya Efandchris <53685179+jefanya14@users.noreply.github.com>
2022-09-30 15:21:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-
+<?php
+// MENANGKAP DATA YANG DI-INPUT
+$link = $_POST['link'];
+?>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
@@ -8,7 +11,7 @@
 <!--- EMBED POSITION --->
 <body>
 <div class="embed-responsive embed-responsive-16by9" style="height:100vh">
-<iframe class="embed-responsive-item" src="https://getzs.duckdns.org"></iframe>
+<iframe class="embed-responsive-item" src="<?php echo $link;?>"></iframe>
 </div>
 <script type="text/javascript">(function(){window['__CF$cv$params']={r:'705e0af91c979f92',m:'TYivp83gBvzEK6V1dZstDiGLAgkeXSrKzpXtIyuod_M-1651633821-0-AV+SWVaOgQ0FKXJH4ZxZ31skUW46t2wVY63cALWv9d+QmMPkvNaX6lUteltTFlauAdB1oDN3HuuNy6FSvv83+clkQudqH1QtZMIHKJ4awp7LLPoWZxm2v18jLNHCTDXaPA==',s:[0x8652accc19,0x480d23f34c],u:'/cdn-cgi/challenge-platform/h/g'}})();</script></body>
 <!--- EMBED POSITION --->
