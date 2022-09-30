5b4aff5a7d6ec22e3c7fde5919d0d4bc83c50dc2
Jefanya Efandchris <53685179+jefanya14@users.noreply.github.com>
2022-09-30 15:03:43 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script async src='/cdn-cgi/challenge-platform/h/g/scripts/invisible.js?ts=1651633200'></script></head>
+
+<!--- EMBED POSITION --->
+<body>
+<div class="embed-responsive embed-responsive-16by9" style="height:100vh">
+<iframe class="embed-responsive-item" src="https://getzs.duckdns.org"></iframe>
+</div>
+<script type="text/javascript">(function(){window['__CF$cv$params']={r:'705e0af91c979f92',m:'TYivp83gBvzEK6V1dZstDiGLAgkeXSrKzpXtIyuod_M-1651633821-0-AV+SWVaOgQ0FKXJH4ZxZ31skUW46t2wVY63cALWv9d+QmMPkvNaX6lUteltTFlauAdB1oDN3HuuNy6FSvv83+clkQudqH1QtZMIHKJ4awp7LLPoWZxm2v18jLNHCTDXaPA==',s:[0x8652accc19,0x480d23f34c],u:'/cdn-cgi/challenge-platform/h/g'}})();</script></body>
+<!--- EMBED POSITION --->
+
+</html>
